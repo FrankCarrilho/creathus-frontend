b2fba4fa905e2a0efc82e336c2d8f4195000af6b
FrankCarrilho <jotah.erri@gmail.com>
2021-02-06 11:14:38 -0400

    Estrutura Montada

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Estrutura />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Estrutura from './components/Estrutura.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Estrutura
   }
 }
 </script>
@@ -25,4 +24,12 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+body {
+    margin-bottom: 60px;
+    background: #314755; /* fallback for old browsers */
+    background: -webkit-linear-gradient(to right, #26a0da, #314755); /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(to right, #26a0da, #314755); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+}
+
 </style>
---
+++ src/components/Estrutura.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="@/assets/logo.png" />
+    <div class="container">
+      <nav class="nav">
+        <div class="nav-wrapper blue darken-1">
+          <h5 class="brand-logo center">Consumindo API</h5>
+        </div>
+      </nav>
+    </div>
+
+    <div class="container">
+      <ul>
+        <li v-for="(erro, index) of errors" :key="index">
+          campo <b>{{ erro.field }}</b> - {{ erro.defaultMessage }}
+        </li>
+      </ul>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Col.1</th>
+            <th>Col.2</th>
+            <th>Col.3</th>
+            <th>Col.4</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>atributo 1</td>
+            <td>atributo 2</td>
+            <td>atributo 3</td>
+            <td>atributo 4</td>
+          </tr>
+        </tbody>
+      </table>
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeComponents',
+  data() {
+    return {
+      atributos: []
+    };
+  },
+};
+</script>
+
+<style scoped>
+nav {
+  white-space: normal;
+  text-align: center;
+  word-break: break-all;
+  color: white;
+}
+
+table {
+  text-align: center;
+  color: white;
+}
+
+.footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  white-space: nowrap;
+  line-height: 60px; /* Vertically center the text there */
+}
+</style>
